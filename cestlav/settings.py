50e48a1b1085a178d129fc23e2c421e87898c490
Ila-bura <>
2023-09-28 21:36:44 +0000

    Added allowed host var

--- cestlav/settings.py
+++ cestlav/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['c-est-la-v.herokuapp.com',
+ALLOWED_HOSTS = ['c-est-la-v-1864b2ffcdf3.herokuapp.com',
                  '8000-ila-bura-c-est-la-v-qedvfrusit.us2.codeanyapp.com']
 
 
